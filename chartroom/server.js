58b7403002b3c27a0d5bf40cc49f80f1937b09c6
dimitar9 <dimitar9909@gmail.com>
2014-02-04 01:19:05 -0500

    chapter 4

--- chartroom/server.js
+++ chartroom/server.js
@@ -1,60 +1,60 @@
 var http = require('http');
-var fs = require('fs');
+var fs  = require('fs');
 var path = require('path');
 var mime = require('mime');
 var cache = {};
 
-function send404(response){
-	console.log("sending 404...");
-	response.writeHead(404, {'Content-Type':'text/plain'});
-	response.write('Error 404: Resource not found.');
-	response.end();
-	console.log("sent 404...");
+function send404(response) {
+  response.writeHead(404, {'Content-Type': 'text/plain'});
+  response.write('Error 404: resource not found.');
+  response.end();
 }
 
 function sendFile(response, filePath, fileContents) {
-	response.writeHead(
-		200,
-		{
-			"content-type":mime.loopup(path.basename(filePath))
-		});
-	response.end(fileContents);	
+  response.writeHead(
+    200, 
+    {"content-type": mime.lookup(path.basename(filePath))}
+  );
+  response.end(fileContents);
 }
 
 function serveStatic(response, cache, absPath) {
-	if(cache[absPath]) {
-		sendFile(response,absPath,cache[absPath]);
-	} else{
-		fs.exists(absPath,function(exists) {
-			if(exists) {
-				fs.readFile(absPath,function(err,data){
-					if(err) {
-						send404(response);
-					} else{
-						cache[absPath] = data;
-						sendFile(response,absPath,data);
-					}
-				});
-			}else {
-				send404(response);
-			}
-		});
-	}
+  if (cache[absPath]) {
+    sendFile(response, absPath, cache[absPath]);
+  } else {
+    fs.exists(absPath, function(exists) {
+      if (exists) {
+        fs.readFile(absPath, function(err, data) {
+          if (err) {
+            send404(response);
+          } else {
+            cache[absPath] = data;
+            sendFile(response, absPath, data);
+          }
+        });
+      } else {
+        send404(response);
+      }
+    });
+  }
 }
 
-var server = http.createServer(function(request,response){
-	var filePath = false;
-	if(request.url == '/') {
-		console.log("getting root path");
-		filePath = 'public/index.html';
-	} else {
-		console.log("getting path");
-		filePath = 'public' + request.url
-	}
-	var absPath = './' + filePath;
-	serveStatic(response,cache,absPath)
-}) ;
+var server = http.createServer(function(request, response) {
+  var filePath = false;
 
-server.listen(3000,function(){
-	console.log("server listening on port 3000.");
+  if (request.url == '/') {
+    filePath = 'public/index.html';
+  } else {
+    filePath = 'public' + request.url;
+  }
+
+  var absPath = './' + filePath;
+  serveStatic(response, cache, absPath);
 });
+
+server.listen(3000, function() {
+  console.log("Server listening on port 3000.");
+});
+
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
--- httpserver.js
+++ httpserver.js
@@ -1,7 +1,33 @@
 var http = require('http');
-
+var url = require('url');
+var items = [];
 var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
+  switch (req.method) {
+  	case 'GET':
+		items.forEach(function(item, i){
+  			res.write(i + ') ' + item + '\n');
+		});
+		res.end();
+		break;
+
+  	case 'POST':
+  		var item = ''
+
+  		req.setEncoding('utf8')
+  		req.on('data',function(chunk) {
+    		console.log(chunk);
+    		item += chunk;
+    	});
+  		req.on('end',function(){
+    		console.log('done parsin');
+    		items.push(item);
+    		res.end('OK\n');
+    	});
+    	break;
+    }
 });
-server.listen(8080);
+
+
+
+
+server.listen(3000);
