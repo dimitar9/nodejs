22574842e7d92c41538b0a587c975bbb3fe3aa40
dimitar9 <dimitar9909@gmail.com>
2014-07-19 00:17:17 -0400

    js

--- load_albums.js
+++ load_albums.js
@@ -1,17 +1,40 @@
 var http = require('http'),
     fs = require('fs');
 
+
+
 function load_album_list(callback) {
-	fs.readdir(
-		"albums",
-		function (err, files) {
-			if (err) {
-				callback(err);
-				return;
-			}
-			callback(null, files);
-		}
-		);
+    fs.readdir(
+        "albums",
+        function (err, files) {
+            if (err) {
+                callback(err);
+                return;
+            }
+
+            var only_dirs = [];
+            (function iterator(index) {
+                if (index == files.length) {
+                    callback(null, only_dirs);
+                    return;
+                }
+
+                fs.stat(
+                    "albums/" + files[index],
+                    function (err, stats) {
+                        if (err) {
+                            callback(err);
+                            return;
+                        }
+                        if (stats.isDirectory()) {
+                            only_dirs.push(files[index]);
+                        }
+                        iterator(index + 1)
+                    }
+                );
+            })(0);
+        }
+    );
 }
 
 function handle_incoming_request(req, res) {
